5ad61438c4b9b8877136d4660c5317e0d83bab9f
roxibi <106161599+roxibi@users.noreply.github.com>
2022-10-14 17:17:13 +0200

    formatting

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
-const btn=document.querySelector('#btn');
-const answer=document.querySelector('#answer');
-const input=document.querySelector('#input');
-
+const btn = document.querySelector("#btn");
+const answer = document.querySelector("#answer");
+const input = document.querySelector("#input");
 
 btn.addEventListener("click", (e) => {
-    e.preventDefault();
-    console.log(input.value);
-    const yearsLeft = 85 - input.value;
-    console.log(yearsLeft);
-    const weeksLeft = yearsLeft * 52;
-    console.log(weeksLeft);
-    answer.innerHTML=weeksLeft;
-       input.value='';
-})
-
-
-
-
-
-
-
-
-
-
-
+  e.preventDefault();
+  console.log(input.value);
+  const yearsLeft = 85 - input.value;
+  console.log(yearsLeft);
+  const weeksLeft = yearsLeft * 52;
+  console.log(weeksLeft);
+  answer.innerHTML = weeksLeft;
+  input.value = "";
+});
