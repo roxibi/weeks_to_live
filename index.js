d9bcecbf19d4a1dd04e4a09383aff318016b386a
roxibi <106161599+roxibi@users.noreply.github.com>
2022-10-14 15:51:15 +0200

    calculating and displaying answer

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const btn=document.querySelector('#btn');
-const answer=document.querySelector('#input');
+const answer=document.querySelector('#answer');
+const input=document.querySelector('#input');
 
 btn.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log(answer.value);
-    return answer.value;
+    console.log(input.value);
+    const yearsLeft = 85 - input.value;
+    const weeksLeft = yearsLeft * 52;
+  
+    answer.innerHTML=weeksLeft;
+
+
+    input.value='';
 })
+
+
+
+
 
 
 
