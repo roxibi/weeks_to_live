de598ab6fd303babee703c45b4884ed64144817b
roxibi <106161599+roxibi@users.noreply.github.com>
2022-10-14 15:33:17 +0200

    event listener and returning input value

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const btn=document.querySelector("#btn");
-const input=document.querySelector("#input").value;
-handleClick();
+const btn=document.querySelector('#btn');
+const answer=document.querySelector('#input');
+
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(answer.value);
+    return answer.value;
+})
+
+
+
+
