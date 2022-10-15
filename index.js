263ea50ece2f7c842b2858c4ce1e9f5750d0eb71
roxibi <106161599+roxibi@users.noreply.github.com>
2022-10-15 22:32:58 +0200

    changing text for over 85

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const btn = document.querySelector("#btn");
 const answer = document.querySelector("#answer");
 const input = document.querySelector("#input");
+const overage = document.querySelector("#overage");
 
 btn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -10,7 +11,7 @@ btn.addEventListener("click", (e) => {
   const weeksLeft = yearsLeft * 52;
   console.log(weeksLeft);
   function cases () {if (input.value>=0 && input.value<=84) {
-    answer.innerHTML = weeksLeft;} else if (input.value>=85) {answer.innerHTML = 'more than enough';}
+    answer.innerHTML = weeksLeft;} else if (input.value>=85) {overage.innerHTML='You have made a lot of precious memories in your life. <br> Be thankful and enjoy them.', answer.innerHTML='';}
     else {answer.innerHTML='???'};  
     console.log(answer);
       input.value = "";}
